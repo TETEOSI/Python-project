3fc12313dfe4fcd0ba67cfb94648a37287010673
TETEOSI <temtial@gmail.com>
2024-03-22 17:37:14 +0100

    python first commit

---
+++ #Ask for user input.py
@@ -0,0 +1,6 @@
+#Ask for user input
+name=input("Enter your name: Temple")
+age=input("Enter your age: 20")
+location=input("Enter your location: Nigeria")
+
+print("[name], I am [age]years old and live in [location].")
